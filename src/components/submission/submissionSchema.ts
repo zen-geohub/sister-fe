465eff28de9a8e48b8d71526e99423e71585fc16
zen-geohub <m008bsy0791@bangkit.academy>
2024-11-10 21:10:01 +0700

    fix: optional scale schema

--- src/components/submission/submissionSchema.ts
+++ src/components/submission/submissionSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const statementSchema = z.object({
   statement: z.string().min(1),
-  scale: z.number().min(1).max(5),
+  scale: z.number().optional(),
   additionalInfo: z.string().optional(),
 });
 
@@ -11,8 +11,8 @@ const categorySchema = z.array(statementSchema);
 export const submissionSchema = z.object({
   Nama: z.string().min(1, "Nama pemohon tidak boleh kosong!"),
   Golongan: z.string().min(1, "Golongan tidak boleh kosong!"),
-  Kecamatan: z.string().min(1),
-  Kelurahan: z.string().min(1),
+  Kecamatan: z.string().min(1, "Pilih lokasi menggunakan peta!"),
+  Kelurahan: z.string().min(1, "Pilih lokasi menggunakan peta!"),
   "Koordinat Lintang": z.number().min(-90).max(90),
   "Koordinat Bujur": z.number().min(-180).max(180),
 
